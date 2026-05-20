dbef53a27ce0f5c6a9273b527ab9199e2369f7a7
Sagar Satish Turuvekere <sturuvek@c4b10c3.42berlin.de>
2026-05-20 18:34:12 +0200

    header file updated

--- libft.h
+++ libft.h
@@ -4,6 +4,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+typedef struct  s_list
+{
+    void    *content;
+    struct s_list   next;
+}   t_list;
+
 int ft_atoi(const char *s);
 void	ft_bzero(void *ptr, size_t n);
 void	*ft_calloc(size_t nmeb, size_t size);
